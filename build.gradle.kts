ec7f23d735c81d6c775e01df5398bcab7016b183
EllieChoi <ellie19981008@gmail.com>
2024-05-23 17:27:12 +0900

    gradle + Oracle dbms connection success

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.oracle.database.jdbc:ojdbc6:11.2.0.4")
 }
 
 tasks.test {
--- src/main/java/mop/Main.java
+++ src/main/java/mop/Main.java
@@ -1,17 +1,46 @@
 package mop;
 
+import java.sql.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        /*
+        // SQL 사용하는 방법 테스트
+         */
+        DatabaseConnect dc = new DatabaseConnect();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Connection conn = null;
+        ResultSet rs = null;
+        String userid = "scott";
+        String passwd = "tiger";
+
+        try {
+
+            conn = dc.serverConnect(userid, passwd);
+
+            String sql = "select deptno, dname, loc from dept";
+
+            rs = dc.getSQLResult(conn, sql);
+
+            while (rs.next()) {
+                int deptno = rs.getInt("deptno");
+                String dname = rs.getString("dname");
+                String loc = rs.getString("loc");
+                System.out.println(deptno + "\t" + dname + "\t" + loc);
+            }
+
+            dc.closeResources(rs, conn);
+
+        } catch (Exception e) {
+            e.printStackTrace();  // Print stack trace to console
+        } finally {
+            try {
+                if (rs != null) rs.close();
+            } catch (Exception e) {
+                e.printStackTrace();  // Print stack trace to console
+            }
         }
     }
 }
